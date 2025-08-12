b4613437117eb960f71cfb01545646009def6964
Jonas Kamsker <jonas.kamsker@suxxesso.com>
2025-08-12 11:18:14 +0200

    Spec Polymorphic

--- src/Serializer.Contracts/SerializeCollectionAttribute.cs
+++ src/Serializer.Contracts/SerializeCollectionAttribute.cs
@@ -10,30 +10,3 @@ public class SerializeCollectionAttribute : Attribute
     public string? CountSizeReference { get; set; }
 }
 
-// Polymorphic attribute:
-// [SerializePolymorphic(nameof(PropertyName))]
-// [PolymorphicOption(1, typeof(Type1))]
-// [PolymorphicOption(2, typeof(Type2))]
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-public class SerializePolymorphicAttribute : Attribute
-{
-    public string? PropertyName { get; set; }
-
-    public SerializePolymorphicAttribute(string? propertyName = null)
-    {
-        PropertyName = propertyName;
-    }
-}
-
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
-public class PolymorphicOptionAttribute : Attribute
-{
-    public int Id { get; }
-    public Type Type { get; }
-
-    public PolymorphicOptionAttribute(int id, Type type)
-    {
-        Id = id;
-        Type = type;
-    }
-}
---
+++ src/Serializer.Contracts/SerializePolymorphicAttribute.cs
@@ -0,0 +1,29 @@
+namespace Serializer.Contracts;
+
+// Polymorphic attribute:
+// [SerializePolymorphic(nameof(PropertyName))]
+// [PolymorphicOption(1, typeof(Type1))]
+// [PolymorphicOption(2, typeof(Type2))]
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class SerializePolymorphicAttribute : Attribute
+{
+    public string? PropertyName { get; set; }
+
+    public SerializePolymorphicAttribute(string? propertyName = null)
+    {
+        PropertyName = propertyName;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+public class PolymorphicOptionAttribute : Attribute
+{
+    public int Id { get; }
+    public Type Type { get; }
+
+    public PolymorphicOptionAttribute(int id, Type type)
+    {
+        Id = id;
+        Type = type;
+    }
+}
